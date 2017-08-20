055b2310cf25d96d10c35a6f9c5843d8a1981703
br0d0 <brodier@gmail.com>
2017-08-20 11:33:58 +1000

    Week 2 exercises complete

---
+++ 02week/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from pprint import pprint
+import pysnmp
+import paramiko
+import my_func
+
+def main():
+    my_func.print_hello()
+
+if __name__ == "__main__":
+    main()
